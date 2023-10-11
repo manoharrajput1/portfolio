5e1710b46bd96e382662aa07178bd6a26a66da92
unknown <bepositivemr@gmail.com>
2023-10-11 13:44:14 +0530

    add

--- app/about/page.js
+++ app/about/page.js
@@ -12,7 +12,7 @@ const About = () => {
                 <div className='grid-rows justify-center self-center '> 
                     <div className='text-yellow-700 font-bold'> Manohar Singh </div><br/>
                     <div> <span className='text-blue-600'>Skills :</span> Html, JavaScript, Css ,Mysql, MongoDB, Python, NextJs, Django </div><br/>
-                    <div> <span className='text-blue-600'>Projects:</span> <a href='https://portliofol.netlify.com' target='_blank'> Personal Portfolio </a>,
+                    <div> <span className='text-blue-600'>Projects:</span> <a href='https://portfolio-silk-one-23.vercel.app/' target='_blank'> Personal Portfolio </a>,
                     <a href='https://manoharsignh.pythonanywhere.com' target='_blank'> Ecommerce Website </a>
                     </div>
                 </div>
