b2aec6251e5d24875160e43559d7e3c25fa0f668
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2023-10-13 01:44:46 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 
 const Home = () => {
   return (
-    <div className="flex justify-center">Welcome Jhalars</div>
+    <div className="flex justify-center">
+        <h1>Welcome Jhalars</h1>
+        <h5>Go Online With The World....</h5>
+    </div>
   )
 }
 
