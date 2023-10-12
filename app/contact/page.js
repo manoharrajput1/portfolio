91fb715ff0c21d9c3b4b641f8f7e16784dc54b34
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2023-10-12 20:56:03 +0530

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -16,6 +16,7 @@ const Contact = () => {
       headers: {
         'Content-Type':'application/json'
       },
+      credentials: "include",
       body: JSON.stringify({
         username: user.username,
         mobile: user.mobile,
