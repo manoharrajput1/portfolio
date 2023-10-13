50eb4c43447a9943026ae2dbb3df82c6074d841d
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2023-10-13 07:05:52 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 
 const Home = () => {
   return (
-    <div className="justify-center mt-2">
+    <div className="grid grid-cols justify-center align-center mt-2">
         <h1>Welcome Jhalars</h1>
         <h5>Go Online With The World....</h5>
     </div>
