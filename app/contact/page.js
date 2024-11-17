c2e083e5221cb7154ba40386bc5dc35444379c00
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2024-11-17 07:54:53 +0530

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -35,13 +35,13 @@ const Contact = () => {
   }
 
   return (
-    <div> Contact Us </div>
+    <div className= m-1 text-blue> Contact Us </div>
     <form className= 'flex text-midnight flex-col items-center m-2'>
-      <input className='m-1 text-blue text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500' type='text' name='username' value={user.username} onChange={setUserData} placeholder='Username' required />
-      <input className='m-1 text-blue text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'  type='number' name='mobile' value={user.mobile} onChange={setUserData} placeholder='Contact Number'required />
-      <input className='m-1 text-blue text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'  type='email' name='email' value={user.email} onChange={setUserData} placeholder='Email Address' required/>
-      <textarea row='5' className='m-1 text-blue text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'  type='text' name='desc' value={user.desc} onChange={setUserData} placeholder='Describe Issue' required/>
-      <button className='m-1 text-blue p-1 w-100 text-center rounded-lg text-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500' onClick={sendData}  type='submit'>Submit</button>
+      <input className='m-1 placeholder-gray text-blue text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500' type='text' name='username' value={user.username} onChange={setUserData} placeholder='Username' required />
+      <input className='m-1 placeholder-gray text-blue text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'  type='number' name='mobile' value={user.mobile} onChange={setUserData} placeholder='Contact Number'required />
+      <input className='m-1 placeholder-gray text-blue text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'  type='email' name='email' value={user.email} onChange={setUserData} placeholder='Email Address' required/>
+      <textarea row='5' className='m-1 placeholder-gray text-blue text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'  type='text' name='desc' value={user.desc} onChange={setUserData} placeholder='Describe Issue' required/>
+      <button className='m-1 placeholder-gray text-blue p-1 w-100 text-center rounded-lg text-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500' onClick={sendData}  type='submit'>Submit</button>
     </form>
   )
 }
