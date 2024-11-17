7a3b4cfeffc9198c6741a9d8c1bde13b081d81fc
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2024-11-17 19:08:52 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,9 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
+      white: '#ffffff',
+      purple: '#3f3cbb',
+      midnight: '#121063',
       'metal': '#565584',
       'tahiti': '#3ab7bf',
       'silver': '#ecebff',
