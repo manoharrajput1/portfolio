5ae7252670361597f0443c778f70c3d49c5404a3
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2024-11-17 20:46:38 +0530

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -36,10 +36,10 @@ const Contact = () => {
 
   return (
     <form className= ' flex flex-col items-center m-2'>
-      <input className='m-1 text-white text-center rounded-lg bg-gradient-to-r from-indigo via-purple to-pink' type='text' name='username' value={user.username} onChange={setUserData} placeholder='Username' required />
-      <input className='m-1 text-white text-center rounded-lg placeholder-blue bg-gradient-to-r from-indigo via-purple to-pink'  type='number' name='mobile' value={user.mobile} onChange={setUserData} placeholder='Contact Number'required />
-      <input className='m-1 text-white text-center rounded-lg placeholder-blue bg-gradient-to-r from-indigo via-purple to-pink'  type='email' name='email' value={user.email} onChange={setUserData} placeholder='Email Address' required/>
-      <textarea row='5' className='m-1 text-white text-center rounded-lg placeholder-blue bg-gradient-to-r from-indigo via-purple to-pink'  type='text' name='desc' value={user.desc} onChange={setUserData} placeholder='Describe Issue' required/>
+      <input className='m-1 text-white placeholder-white text-center rounded-lg bg-gradient-to-r from-indigo via-purple to-pink' type='text' name='username' value={user.username} onChange={setUserData} placeholder='Username' required />
+      <input className='m-1 text-white placeholder-white text-center rounded-lg placeholder-blue bg-gradient-to-r from-indigo via-purple to-pink'  type='number' name='mobile' value={user.mobile} onChange={setUserData} placeholder='Contact Number'required />
+      <input className='m-1 text-white placeholder-white text-center rounded-lg placeholder-blue bg-gradient-to-r from-indigo via-purple to-pink'  type='email' name='email' value={user.email} onChange={setUserData} placeholder='Email Address' required/>
+      <textarea row='5' className='m-1 text-white placeholder-white text-center rounded-lg placeholder-blue bg-gradient-to-r from-indigo via-purple to-pink'  type='text' name='desc' value={user.desc} onChange={setUserData} placeholder='Describe Issue' required/>
       <button className='m-1 p-1 text-blue w-100 text-center rounded-lg ' onClick={sendData}  type='submit'>Submit</button>
     </form>
   )
