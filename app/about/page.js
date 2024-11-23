aba34be1a415e2ce9d018674aed3a66a4a04fe25
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2024-11-23 19:22:42 +0530

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -18,7 +18,7 @@ const About = () => {
                     <div className='m-1'>
                         <span className="text-blue-600 font-bold">
                             Skills : 
-                        </span>
+                        </span> &nbsp
                         Html, JavaScript, Css ,Mysql, MongoDB, Python, NextJs,
                         Django
                     </div>
@@ -27,14 +27,14 @@ const About = () => {
                         
                         <span className="text-blue-600 font-bold">
                             Projects : 
-                        </span>
+                        </span> &nbsp
                         <a
                             className="text-green"
                             href="https://portfolio-nine-iota-78.vercel.app"
                             target="_blank"
                         >
                             
-                            Personal Portfolio{" "}
+                            Personal Portfolio
                         </a>
                         ,
                         <a
