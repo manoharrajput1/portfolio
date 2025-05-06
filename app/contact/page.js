d4edb6cd4703f8377d3cd7d7acb792676945fa40
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2025-05-06 17:08:11 +0530

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -35,7 +35,7 @@ const Contact = () => {
   }
 
   return (
-    <form className= 'container'>
+    <form className= 'mx-auto'>
       <input className='container-md mx-auto text-white placeholder-white text-center rounded-lg bg-gradient-to-r from-indigo via-purple to-pink' type='text' name='username' value={user.username} onChange={setUserData} placeholder='Username' required />
       <input className='container-md mx-auto text-white placeholder-white text-center rounded-lg placeholder-blue bg-gradient-to-r from-indigo via-purple to-pink'  type='number' name='mobile' value={user.mobile} onChange={setUserData} placeholder='Contact Number'required />
       <input className='container-md mx-auto text-white placeholder-white text-center rounded-lg placeholder-blue bg-gradient-to-r from-indigo via-purple to-pink'  type='email' name='email' value={user.email} onChange={setUserData} placeholder='Email Address' required/>
