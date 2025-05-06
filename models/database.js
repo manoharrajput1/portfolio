9213eb30ae67d51d4501c52f9085d4b33c747587
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2025-05-06 14:14:22 +0530

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,5 +1,5 @@
 import {connect} from 'mongoose'
 
-const conn = async function(){await connect("mongodb+srv://jhalar:lonka01@cluster0.u5gap.mongodb.net/webdev?retryWrites=true&w=majority&appName=Cluster0")
+const conn = async function(){await connect("mongodb+srv://newuser:singh01@cluster0.iiaul.mongodb.net/webdev?retryWrites=true&w=majority&appName=Cluster0")
 }
 module.exports = conn
