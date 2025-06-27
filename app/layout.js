9fd047da27b3f5087cfc5b00a8835434cd287ba3
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2025-06-27 11:00:50 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,10 @@ import Providers from '@/components/Providers'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title> Web Developement </title>
+        <meta name='description' content='web development, website design, MEAN Stack, MERN Stack, frontend web development, backend web development, fullstack web development, html, css, javascript, nodejs, python, reactjs, angularjs, django, flask, mongodb, mysql, mongoose, postgresql, expressjs, nextjs,tailwindcss'>
+      </head>
       <body>
         <Providers children={children}>
           <Navbar />
