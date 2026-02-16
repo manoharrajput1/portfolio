76920c4c4282b3f8b31f4aff01cee6f380acc18f
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2026-02-16 15:37:19 +0530

    Fix connection string in database.js

--- models/database.js
+++ models/database.js
@@ -1,5 +1,5 @@
 import {connect} from 'mongoose'
 
-const conn = async function(){await connect("mongodb+srv://manohar:singhms@cluster0.iiaul.mongodb.net/nextdev?retryWrites=true&w=majority&appName=Cluster0")
+const conn = async function(){await connect("mongodb+srv://manohar:singhms@cluster0.iiaul.mongodb.net/nextdb?rbetryWrites=true&w=majority&appName=Cluster0")
 }
 module.exports = conn
